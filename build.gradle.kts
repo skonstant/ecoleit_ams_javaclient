7d647fcf8ee166fb7c48c4800a22e5ccf1648436
Stéphane Konstantaropoulos <stephane.konstantaropoulos@outpost24.com>
2024-12-02 17:07:08 +0100

    setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,8 @@ repositories {
 }
 
 dependencies {
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
